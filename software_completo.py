7cf138eeec42b4f2185e68dba034724ac1d8759a
UPO-Sevilla-Fco-Javier-Lobo-Cabrera <francisco.lobo6@gmail.com>
2017-05-10 16:35:41 +0200

    Create software_completo.py

---
+++ software_completo.py
@@ -0,0 +1,12 @@
+import os
+
+print "Bienvenido, introduzca (1) si desea solo generar un modelo lineal o (2) si desea testear unos datos frente al modelo generado"
+
+decision = raw_input()
+
+if decision == "1":
+    os.system("python parte1.py")
+
+if decision == "2:
+    os.system("python parte1.py)
+    os.system("python parte2.py)
