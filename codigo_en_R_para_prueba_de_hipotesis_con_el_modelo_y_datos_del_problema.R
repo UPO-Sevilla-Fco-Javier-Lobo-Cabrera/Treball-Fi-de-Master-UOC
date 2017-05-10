4f6593679f9339aad6ce68c26b9d65df49f5e002
UPO-Sevilla-Fco-Javier-Lobo-Cabrera <francisco.lobo6@gmail.com>
2017-05-10 16:32:41 +0200

    Update codigo_en_R_para_prueba_de_hipotesis_con_el_modelo_y_datos_del_problema.R

--- codigo_en_R_para_prueba_de_hipotesis_con_el_modelo_y_datos_del_problema.R
+++ codigo_en_R_para_prueba_de_hipotesis_con_el_modelo_y_datos_del_problema.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #Este script de R generará un fichero denominado 
-#"resultado_se_rechaza_hipótesis.txt"o bien un 
+#"resultado_se_rechaza_hipótesis.txt" o bien un 
 #fichero llamado
 #"resultado_NO_se_rechaza_hipótesis.txt". Se dará 
 #el primer caso si se rechaza que los datos de la 
@@ -14,11 +14,10 @@
 #un fichero. Dicho fichero ha de contener en primer 
 #lugar la desviación típica del modelo lineal y a 
 #continuación los parámetros β en el orden β0, β1,
-#β2, β3 (los dos últimos solo en el caso 
-#de que existan) :
+#y β2:
 
 parametros_mod_lin <- scan(file = "modelo_lineal.txt",
-                     what = double(),  n = -4, sep = "")
+                     what = double(),  n = -3, sep = "")
 
 #Se guarda la desviación típica del modelo lineal en:
 desv_tip_mod_lin <- parametros_mod_lin[1]
@@ -26,30 +25,27 @@ desv_tip_mod_lin <- parametros_mod_lin[1]
 #Se guardan los parámetros β del modelo lineal en:
 parametros_beta_mod_lin <- parametros_mod_lin[2:length(parametros_mod_lin)]
 
-
 #Paso 2:
 #Se procede al conteo de parámetros β del modelo lineal:
-
 num_parametros_beta_mod_lin <- length(parametros_beta_mod_lin) 
-
 
 
 #Paso 3:
 #Lectura de datos del problema. Se espera que los datos del 
 #problema estén contenidos en un fichero. Dicho fichero ha
-#de contener la ΔG y la Δλ del problema:
+#de contener la Kd y lambda del problema:
 
 datos_problema <- scan(file = "problema.txt", 
                        what = double(),  n = -2, sep = "")
 
 
-#Se guarda el ΔG del problema en:
+#Se guarda la Kd del problema en:
 
-ΔG_problema <- datos_problema[1]
+Kd_problema <- datos_problema[1]
 
-#Se guarda el Δλ del problema en:
+#Se guarda el parámetro lambda del problema en:
 
-Δλ_problema <- datos_problema[2]
+lambda_problema <- datos_problema[2]
 
 
 
@@ -57,74 +53,36 @@ datos_problema <- scan(file = "problema.txt",
 #Para testear si los datos del problema se ajustan al modelo
 #lineal se va a seguir el siguiente procedimiento: 
 
-#a)Primero se va a calcular el ΔG predicho por el modelo de acuerdo
-#al dato de Δλ del problema. Se va a suponer que dicho ΔG calculado
+#a)Primero se va a calcular el Kd predicho por el modelo de acuerdo
+#al dato de lambda del problema. Se va a suponer que dicho Kd calculado
 #es una variable aleatoria que sigue una función de 
-#probabilidad normal (dicha suposición está apoyada por el
-#Teorema Central del Límite*). Esta variable aleatoria tiene como
-#media el ΔG predicho por el modelo. Como desviación típica tiene
+#probabilidad normal. Esta variable aleatoria tiene como
+#media el Kd predicho por el modelo. Como desviación típica tiene
 #aquella característica del modelo lineal, la cual se suministró en 
 #el fichero modelo_lineal.txt .
 
 #b)A continuación, se va a realizar un test de t de Student para
-#rechazar o no que el ΔG del problema sea un valor probable del
-#ΔG predicho por el modelo.
-
-#(*)La calorimetría isoterma de titulación es una técnica 
-#basada en el estudio de numerosos complejos idénticos
-#proteína-proteína. Por tanto, puede considerarse la medición
-#final como la suma de múltiples variables aleatorias 
-#independientes equivalentemente distribuidas. De este modo,
-#la función de probabilidad de las mediciones de la técnica
-#ha de seguir una distribución normal, de acuerdo al Teorema 
-#Central del Límite.
+#rechazar o no que el Kd del problema sea un valor probable del
+#Kd predicho por el modelo.
 
 
-#a)Cálculo de ΔG predicho por el modelo de acuerdo
-#al dato de Δλ del problema:
+#a)Cálculo de Kd predicho por el modelo de acuerdo
+#al dato de lambda del problema:
 
-if(num_parametros_beta_mod_lin == 2) #caso de modelo de recta 
-{                                    #( y = b + a*x )
-
-  ΔG_predicho_mod_lin <- parametros_beta_mod_lin[1] + 
-                               parametros_beta_mod_lin[2] * 
-                              Δλ_problema
-}
-
-if(num_parametros_beta_mod_lin == 3) #caso de modelo parabólico
-{                               #( y = b + a*x + c*x² )
-
-  ΔG_predicho_mod_lin <- parametros_beta_mod_lin[1] + 
+Kd_predicho_mod_lin <- parametros_beta_mod_lin[1] + 
                               parametros_beta_mod_lin[2] * 
-                             Δλ_problema +
+                             lambda_problema +
                               parametros_beta_mod_lin[3] * 
-                              (Δλ_predicho_mod_lin_inicial^2)
+                              lambda_problema
     
-}
-
-
-
-if(num_parametros_beta_mod_lin == 4) #( y = b + a*x + c*x² + d*x³)
-{                             
-  
-  ΔG_predicho_mod_lin <- parametros_beta_mod_lin[1] + 
-                              parametros_beta_mod_lin[2] * 
-                             Δλ_problema +
-                              parametros_beta_mod_lin[3] * 
-                              (Δλ_predicho_mod_lin_inicial^2) +
-                              parametros_beta_mod_lin[4] * 
-                              (Δλ_predicho_mod_lin_inicial^3)
-  
-} 
-
 
 #b)Test de t de Student:
 #En la fórmula del estadístico t de Student se está sustituyendo 
 #la cuasidesvación típica muestral por la desviación típica 
 #poblacional (ya que esta última se conoce):
 
-t <- (ΔG_problema - ΔG_predicho_mod_lin) /
-     (desv_tip_mod_lin / sqrt(length(ΔG_problema)))
+t <- (Kd_problema - Kd_predicho_mod_lin) /
+     (desv_tip_mod_lin / sqrt(length(Kd_problema)))
 
 #A continuación se va a calcular la probabilidad acumulada del 
 #estadistico t calculado. Se desconoce el tamaño muestral. Es
@@ -139,23 +97,15 @@ t_probabilidad_acumulada <- pt(t, df = 2)
 
 
 #Si la probabilidad acumulada inferior del 5% o bien superior al
-#95% entonces se rechazará la hipótesis con alfa = 0.05 **. Es decir,
+#95% entonces se rechazará la hipótesis con alfa = 0.05. Es decir,
 #se rechazará que los datos del problema se ajusten al modelo lineal:
 
 
 if ((t_probabilidad_acumulada < 0.05) | (t_probabilidad_acumulada > 0.95))
 {
-  
-  file.create("resultado_se_rechaza_hipótesis.txt", showWarnings = TRUE)
-  
+  write("SI", file = "sobre_el_rechazo_de_la_hipotesis.txt")
 }else
 {
-  file.create("resultado_NO_se_rechaza_hipótesis.txt", showWarnings = TRUE)
-  
-}  
-
-
-
-#(**)En una futura versión del programa, el nivel de significación 
-#alfa a emplear podría ser introducido por el usuario.
+  write("NO", file = "sobre_el_rechazo_de_la_hipotesis.txt")
+}
 
