598b79093e87878ae59839d50620d9262649282c
UPO-Sevilla-Fco-Javier-Lobo-Cabrera <francisco.lobo6@gmail.com>
2017-05-10 13:41:14 +0200

    Update codigo_en_R_para_generar_modelo_a_partir_de_diferencia_de_lambda_y_de_diferencia_de_energia_libre.R

--- codigo_en_R_para_generar_modelo_a_partir_de_diferencia_de_lambda_y_de_diferencia_de_energia_libre.R
+++ codigo_en_R_para_generar_modelo_a_partir_de_diferencia_de_lambda_y_de_diferencia_de_energia_libre.R
@@ -1,67 +1,29 @@
 #!/usr/bin/env Rscript
 
-#Este script está diseñado para ser ejecutado desde una terminal 
-#(véase http://stackoverflow.com/questions/18306362/run-r-script-from-command-line)
-#o en este caso, desde un programa en python que posibilite su ejecución mediante
-#el empleo del método os.system() .
-
-#Este script de R leerá los valores de Δλ y ΔG a partir de los ficheros diferencia_lambda.txt y gibbs.txt
-#generados por otras partes del software. El fichero fichero_lambda.txt debe constar de una única 
-#columna con los valores de Δλ para cada proteína. Por su parte, el fichero gibbs.txt
-#ha de recoger en una única columna las variaciones de energía libre de Gibbs (ΔG) asociadas
-#para cada proteína. Como en cada complejo existen dos proteínas, los valores del fichero
-#gibbs.txt serán repetitivos de dos en dos. PE:
-#5.987
-#5.987
-#7.665
-#7.665
-#...
+#Este script de R leerá los valores de λ y constante de disociación (Kd) a partir de los ficheros lambda.txt y kd.txt
+#generados por otras partes del software. El fichero lambda.txt debe constar de una única 
+#columna con los valores de lambda para cada proteína. Por su parte, el fichero Kd.txt
+#ha de recoger en una única columna la constante de disociación asociada con cada complejo.
 
 
-
-
-
-#La variable respuesta sería ΔG y la variables explicativas serían Δλ, Δλ^2 y Δλ^3 . 
-
+#La variable respuesta sería Kd y la variables explicativas serían lambda, lambda^2 y lambda^3 . 
 
 #Lectura de los parámetros:
 
-lectura_Δλ <- read.table("RUTA_DONDE_ESTE_EL_PROGRAMA_DE_PYTHON/diferencia_lambda.txt")
+lectura_lambda <- read.table("lambda.txt")
 
-Δλ <- lectura_Δλ$V1
-
-
-lectura_ΔG <- read.table("RUTA_DONDE_ESTE_EL_PROGRAMA_DE_PYTHON/gibbs.txt")
-
-ΔG <- lectura_ΔG$V1
+lambda <- lectura_lambda$V1
 
 
-#Creación de los modelos lineales:
+lectura_Kd <- read.table("Kd.txt")
 
-modelo_lineal_a <- lm(ΔG ~ Δλ)
+Kd <- lectura_Kd$V1
 
-modelo_lineal_b <- lm(ΔG ~ Δλ^2)
+#Creación del modelo lineal:
+lambda_cuadrado <- lambda ^2
+modelo_lineal <- lm(Kd ~ lambda_cuadrado + lambda)
 
-modelo_lineal_c <- lm(ΔG ~ Δλ + Δλ^2 + Δλ^3)
-
-
-
-#A continuación se procede al cálculo de R² para cada modelo:
-
-r_cuadrado_modelo_lineal_a <- summary(modelo_lineal_a)$r.squared
-
-r_cuadrado_modelo_lineal_c <- summary(modelo_lineal_b)$r.squared
-
-r_cuadrado_modelo_lineal_c <- summary(modelo_lineal_c)$r.squared
-
-
-
-
-
-
-
-
-
-
+#A continuación se imprime el modelo lineal:
+print(modelo_lineal)
 
 
